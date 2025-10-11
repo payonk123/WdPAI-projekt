0da92a9a668d8797776d14e029bf71b0905b6afa
payonk123 <n.andrzejewska27@gmail.com>
2025-10-11 11:58:18 +0200

    dashboard, login

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
+ <?php
 
-echo "<h1>Hello world 💪</h1>";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+// var_dump($path);
+
+Routing::run($path);
+?>
+ 
